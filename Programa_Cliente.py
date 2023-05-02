383da1b3d4f6ced16ba8ac15227f19ff2d434a42
rianrodrigz <85793781+rianrodrigz@users.noreply.github.com>
2023-05-02 11:18:54 -0300

    Update Programa_Cliente.py

--- Programa_Cliente.py
+++ Programa_Cliente.py
@@ -2,28 +2,26 @@ import socket
 import random
 import time
 
-HOST = 'localhost' 
-PORTA = 5000 
+HOST = 'localhost' #Nome da maquina 
+
+PORTA = 5000 #Porta de conexao 
 
 while True:
     
-    Conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    Conn.connect((HOST, PORTA))
-
+    Conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)#Familia de protocolo: IPV4 e Protocolo:TCP.
     
+    Conn.connect((HOST, PORTA))#Pedindo conexao ao servidor.
+
     Num = random.randint(1, 10**30)
-
     
-    Conn.send(str(Num).encode())
+    #Num = random.randint(1, 11)#Para testar a condicional caso o numero tenha menos de 10 casas.
 
-    
-    resposta = Conn.recv(1024).decode()
+    Conn.send(str(Num).encode())#Encode e utilizado para garantir que vai enviar a mensagem devidamente.
 
-    
-    print(resposta)
+    resposta = Conn.recv(1024).decode()#Decode e utlizado para receber a mesagem devidamente. 
 
-    
-    Conn.close()
+    print(resposta)#Mostra a resposta
 
-    
-    time.sleep(10)
+    Conn.close()#Fecha a conexao
+
+    time.sleep(10)#Utilizando a biblioteca time para esperar os 10 segundos ate a proxima conexao
