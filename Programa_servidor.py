dd885a99453df638658f2d49c0a33522471b794e
rianrodrigz <85793781+rianrodrigz@users.noreply.github.com>
2023-05-02 11:25:11 -0300

    Update Programa_servidor.py

--- Programa_servidor.py
+++ Programa_servidor.py
@@ -1,9 +1,9 @@
 import socket
 import random
 
-HOST = 'localhost' # Nome da maquina
+HOST = 'localhost' # Nome da maquina.
 
-PORTA = 5000 # Porta de conexao
+PORTA = 5000 # Porta de conexao.
 
 serv = socket.socket(socket.AF_INET, socket.SOCK_STREAM)#Familia de protocolo: IPV4 e Protocolo:TCP.
 
@@ -14,7 +14,7 @@ serv.listen(1)#Modo de escuta.
 while True:
     print('Aguardando conexão do Cliente')
     
-    Conn, Ende = serv.accept() #Metodo para aceitar a conexao
+    Conn, Ende = serv.accept() #Metodo para aceitar a conexao.
 
     print(f'Conectado em:{Ende}')
 
@@ -22,18 +22,18 @@ while True:
 
     num_casas = len(str(Num)) #Checa o numero recebido contem.
 
-    if num_casas > 10: #Condicional para caso tenha mais de 10 casas ele retorna uma string do mesmo tamanho 
+    if num_casas > 10: #Condicional para caso tenha mais de 10 casas ele retorna uma string do mesmo tamanho. 
         
-        string = ''.join(random.choices('abcdefghijklmnopqrstuvwxyz', k=num_casas))
+        string = ''.join(random.choices('abcdefghijklmnopqrstuvwxyz', k=num_casas)) #Retorna uma lista do tamanho de k de elementos.
 
         Conn.send(string.encode())
-    else:#Condicional para caso tenha menos de 10 casas, o programa retornara se o numero e impar ou par 
+    else:#Condicional para caso tenha menos de 10 casas, o programa retornara se o numero e impar ou par. 
         
         if Num % 2 == 0:
             resposta = "PAR"
         else:
             resposta = "ÍMPAR"
 
-        Conn.send(resposta.encode()) # Envia resposta para o Cliente
+        Conn.send(resposta.encode()) # Envia resposta para o Cliente.
 
-    Conn.close() #Fecha a conexao   
+    Conn.close() #Fecha a conexao.   
